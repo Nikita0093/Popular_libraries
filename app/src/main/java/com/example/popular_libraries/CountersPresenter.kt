7bc716d2b3840d630ae2024ef81287697597a597
Nikita Dyachenko <nikita.dyachenko93@bk.ru>
2022-12-18 22:54:10 +0300

    Урок 1 - Урок окончен

--- app/src/main/java/com/example/popular_libraries/CountersPresenter.kt
+++ app/src/main/java/com/example/popular_libraries/CountersPresenter.kt
@@ -4,20 +4,17 @@ class CountersPresenter(private val view: MainView) {
 
     private val model = CountersModel()
 
-    fun onCounterClick(id: Int){
+    fun onCounterClick(id: Int) {
 
-        when(id){
+        when (id) {
             R.id.btnNumberOne -> {
-                val newValue = model.next(0)
-                view.setText(newValue.toString(),0)
+                view.setText(model.next(0).toString(), 0)
             }
-            R.id.btnNumberTwo-> {
-                val newValue = model.next(1)
-                view.setText(newValue.toString(),1)
+            R.id.btnNumberTwo -> {
+                view.setText(model.next(1).toString(), 1)
             }
             R.id.btnNumberThree -> {
-                val newValue = model.next(2)
-                view.setText(newValue.toString(),2)
+                view.setText(model.next(2).toString(), 2)
             }
         }
     }
